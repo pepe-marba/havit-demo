3a5ff6dc3a742c68074fdbe34c9ac7fb94e8baf8
Pepe MB <ppmb.dev@gmail.com>
2022-05-27 14:06:59 -0500

    Sorting items with HxAccordion element

---
+++ havit-sorting-demo/Program.cs
@@ -0,0 +1,14 @@
+using havit_sorting_demo;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Havit.Blazor.Components.Web;
+using Havit.Blazor.Components.Web.Bootstrap;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddHxServices();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
